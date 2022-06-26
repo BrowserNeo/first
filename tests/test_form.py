06e8da71a698944045b1b8fc09c0b35349e692cb
George BrowserNeo <disconnect>
2022-06-26 18:20:37 +0300

    change test_form

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,22 +1,35 @@
-from selene import have, command
+from selene import have
 from selene.support.shared import browser
+
+firstname = 'Avraam'
+lastname = 'Neo'
+email = 'figaro@gmail.com'
+gender = 'Male'
+usernumber = '1234567890'
+hobbies = 'Music'
+currentaddress = 'Neverland'
+data = '22 Jun 1990'
 
 
 def test_form():
     browser.open('/')
 
 
-    browser.element('#firstName').type('Avraam')
-    browser.element('#lastName').type('Neo')
-    browser.element('#userEmail').type('figaro@gmail.com')
-    browser.element('#userNumber').type('1234567890')
-    browser.element('#currentAddress').type('Neverland')
+    browser.element('#firstName').type(firstname)
+    browser.element('#lastName').type(lastname)
+    browser.element('#userEmail').type(email)
+    browser.element('#userNumber').type(usernumber)
+    browser.element('#currentAddress').type(currentaddress)
 
-    browser.element('[class="custom-control-label"]').should(have.exact_text('Male')).click()
+    browser.element('[class="custom-control-label"]').should(have.exact_text(gender)).click()
 
-    browser.element('[class="custom-control-label"][for="hobbies-checkbox-3"]').should(have.exact_text('Music')).click()
+    browser.element('[class="custom-control-label"][for="hobbies-checkbox-3"]').should(have.exact_text(hobbies)).click()
 
-    browser.element('')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('#dateOfBirthInput').type(data)
+
+
+
 
 
 
