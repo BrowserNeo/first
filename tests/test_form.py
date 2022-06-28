6cafed9e6b62bcfa2342bf45f5ff73943e35eeec
George BrowserNeo <disconnect>
2022-06-28 20:01:37 +0300

    change test_form

--- tests/test_form.py
+++ tests/test_form.py
@@ -10,6 +10,7 @@ hobbies = 'Music'
 currentaddress = 'Neverland'
 data = '22 Jun 1990'
 month = 'April'
+state = 'NCR'
 
 
 def test_form():
@@ -22,16 +23,27 @@ def test_form():
     browser.element('#userNumber').type(usernumber)
     browser.element('#currentAddress').type(currentaddress)
 
-    browser.element('[class="custom-control-label"]').should(have.exact_text(gender)).click()
+    browser.element('[class~="custom-control-label"]').should(have.exact_text(gender)).click()
 
-    browser.element('[class="custom-control-label"][for="hobbies-checkbox-3"]').should(have.exact_text(hobbies)).click()
+    browser.element('[class~="custom-control-label"][for="hobbies-checkbox-3"]').should(have.exact_text(hobbies)).click()
 
     browser.element('#dateOfBirthInput').click()
 
-    browser.element('[class="react-datepicker__month-select"][contains(.//text(), month)]').click()
+    browser.element('[class~="react-datepicker__month-select"]').element('[value="8"]').click()
+
+    browser.element('[class~="react-datepicker__year-select"]').element('[value="1994"]').click()
+
+    browser.element('[class~="react-datepicker__day--015"]').click()
+
+    # Дата рождения - сделал костыльно, т.к. не понял как работает clear() в этой графе
+
+    browser.element('#subjectsContainer').click().should(have.exact_text("History")).click()
+
+    # Не сделал!!! Current address
+
+    browser.element('#state').click().should(have.exact_text(state)).click()
 
 
-    # Дата рождения - пока не работает (план б - кликами)
 
 
 
